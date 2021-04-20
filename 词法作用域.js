8fcb8aa679edbc19961d7a3188b1a2a88d27ffae
zhengllingling <1072828530@qq.com>
2021-04-20 08:59:16 +0800

    feat 词法作用域的理解

--- 词法作用域.js
+++ 词法作用域.js
@@ -12,3 +12,29 @@ function bar() {
     foo();
 }
 bar();
+
+/** 
+ * 下面两段代码是典型的词法作用域的例子
+ * 因为函数作用域在函数定义的时候就决定了
+ * 所以下面两段代码返回的都是local scope
+ * */ 
+
+var scope = "global scope"
+function checkscope() {
+    var scope = "local scope"
+    function f() {
+        return scope;
+    }
+    return f();
+}
+console.log(checkscope())
+
+var scope = "global scope"
+function checkscope() {
+    var scope = "local scope"
+    function f() {
+        return scope;
+    }
+    return f;
+}
+console.log(checkscope()())
