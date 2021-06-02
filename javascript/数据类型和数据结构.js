495537d1cb316605c843c5b53ac1c47d52344e21
zhengllingling <1072828530@qq.com>
2021-06-02 09:39:38 +0800

    fix 数据类型和数据结构 类型判断修改

--- javascript/数据类型和数据结构.js
+++ javascript/数据类型和数据结构.js
@@ -52,4 +52,46 @@ const a2 = BigInt(1);
 
 // 8.Object 一组键值对集合
 // es6对象新增方法
-Object.is() // 同值相等算法 
+/** 
+ * 比较方法： 
+ * 1. == 会做类型转换
+ * 2. === 不会做类型转换 但是-0等于0 NaN不等于NaN
+ * */
+Object.is() // 同值相等算法 -0不等于0 NaN等于NaN
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+// typeof 不能区分null和内置类型和用户自定义类型
+typeof undefined // undefined
+typeof null // object
+typeof 1 // number
+typeof "aa" // string
+typeof Symbol("1") // symbol
+typeof 1n // bigint
+typeof false // boolean
+typeof {} // object
+typeof new Date() // object
+typeof function(){} // function
+typeof new Person("tony", 12) // object
+
+const p1 = new Person("tony", 12);
+
+// Object.property.toString() 可以区分基本类型、引用类型和内置类型 但是不能区分用户自定义类是具体哪一类
+// 基本类型没有toString方法可以使用call方法改变toString函数中的this指向，从而使得基本类型也可以使用toString方法
+// 调用该方法会返回[object + class]形式的字符串 class就是类型
+Object.prototype.toString.call("aa"); // [object String] 
+function type(val) {
+    if(typeof val === "object") {
+        const result = Object.prototype.toString.call(val);
+        return result.substring(8, result.length-1).toLowerCase();
+    }
+    return typeof val;
+}
+type(new Person("p1")) // Object
+
+// instanceof 判断具体哪一个类型 
+
+p1 instanceof Person // true
